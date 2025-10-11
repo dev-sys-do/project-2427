54802e6bff5c651d5aca91356d5c3651646d4571
axel.frache <axel.frache@etu.umontpellier.fr>
2025-10-11 02:12:20 +0200

    chore: initialize tic-tac-toe Rust project
    Signed-off-by: axelfrache <axelfrache@gmail.com>

---
+++ topics/tic-tac-toe/src/main.rs
@@ -0,0 +1,10 @@
+/// Tic-Tac-Toe game with AI opponent using Minimax algorithm
+///
+/// This program allows a human player to play against an AI opponent
+/// that uses the Minimax algorithm to play optimally.
+fn main() {
+    println!("Welcome to Tic-Tac-Toe!");
+    println!("Play against an unbeatable AI opponent.");
+    println!();
+    println!("Game coming soon...");
+}
