5e4b5f4d2628f8f3e89301e768df928910c9d14e
Liam SOULET <liamsoulet@gmail.com>
2025-10-23 15:48:14 +0200

    feat: add board and player sructs
    Signed-off-by: Liam SOULET <liamsoulet@gmail.com>

--- topics/tic-tac-toe/src/main.rs
+++ topics/tic-tac-toe/src/main.rs
@@ -1,3 +1,21 @@
+mod board;
+
+use board::{Board, Player};
+
 fn main() {
-    println!("Hello, world!");
+    println!("=== Tic-Tac-Toe Board Demo ===\n");
+    
+    let mut board = Board::new();
+    
+    println!("Empty board:");
+    board.display();
+    
+    // Demo: Place some moves to show the board in action
+    println!("Demo: Placing some moves...");
+    board.place_move(5, Player::X);
+    board.place_move(1, Player::O);
+    board.place_move(9, Player::X);
+    board.display();
+    
+    println!("Run 'cargo test' to see all the unit tests for the board functionality!");
 }
