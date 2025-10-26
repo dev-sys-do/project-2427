e23031bb1cd0db50f9155ea269d0333dcad7e260
rrouviere <ruben.rouviere@etu.umontpellier.fr>
2025-10-26 17:40:26 +0100

    chore: Initialize the cargo project + basic outline
    Signed-off-by: rrouviere <ruben.rouviere@etu.umontpellier.fr>

---
+++ topics/p2p-transfer-protocol/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // Parse arguments
+    // Go to server or client mode
+}
+
+
+fn server_mode() {
+    // Start server
+    // Bind to port
+    // Open output file
+    // Wait for client
+    // Receive & save file
+}
+
+fn client_mode() {
+    // Start client
+    // Connect to server
+    // Open input file
+    // Send input file
+    // Show result
+}
