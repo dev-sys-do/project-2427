b336c4316de2bea1813dbf601ab3c12d73ac4aec
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-11-02 22:29:28 +0100

    doc: add documentation to PlayerBehavior methods
    Signed-off-by: Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>

--- topics/tic-tac-toe/src/player/mod.rs
+++ topics/tic-tac-toe/src/player/mod.rs
@@ -5,7 +5,11 @@ pub mod terminal;
 
 /// Represents a player that can play a [`crate::logic::game::Game`]
 pub trait PlayerBehavior {
+    /// Called by Game() when the game starts
     fn game_start(&mut self, me: PlayerID);
+    /// Called by Game() to get the player's next move. Implementation should return
+    /// a valid (free and 0-8) position, or play() will be called again with the same grid. 
     fn play(&mut self, grid: Grid) -> crate::Result<Position>;
+    /// Called by Game() when the game ends
     fn game_ended(&mut self, grid: Grid, winner: Option<PlayerID>);
 }
